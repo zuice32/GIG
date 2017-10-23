3ac117e11f0d2cc4764228d54c848af88c5bdb4c
Peter Monks <monksp@monksp.org>
2017-10-22 23:30:31 -0400

    removed no longer used header component

--- src/Components/Header/Header.test.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import Header from './Header';
-
-import renderer from 'react-test-renderer';
-
-it('renders without crashing', () => {
-  const rendered = renderer.create(<Header />).toJSON();
-  expect(rendered).toBeTruthy();
-});
