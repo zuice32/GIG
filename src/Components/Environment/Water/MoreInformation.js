478c940f6f2bd0768d2b6d3c506a698836515c5c
Peter Monks <monksp@monksp.org>
2017-10-26 15:07:07 -0400

    turned water links into actual links

--- src/Components/Environment/Water/MoreInformation.js
+++ src/Components/Environment/Water/MoreInformation.js
@@ -4,14 +4,10 @@ import {
   Separator,
 } from 'native-base';
 
+import Link from '../../Common/Link';
+
 export default class MoreInformation extends Component {
   render() {
-    //http://padep-1.maps.arcgis.com/apps/Cascade/index.html?appid=d3ad8b96c3344a2aaeb7e6f1d310329
-    //http://www.dep.pa.gov/DataandTools/Webinars/Pages/Water-Management.aspx
-    //http://www.padep-1.maps.arcgis.com/apps/Cascade/index.html?appid=226b2b211887475394097b99797a1a25
-    //http://www.depgis.state.pa.us/WQN/
-    //http://www.dep.pa.gov/Business/Water/CleanWater/WaterQuality/Pages/Water-Quality-Network.aspx
-    //http://data.pa.gov
     return (
       <List>
         <ListItem>
@@ -20,22 +16,34 @@ export default class MoreInformation extends Component {
         </ListItem>
         <Separator bordered><Text>More Information</Text></Separator>
         <ListItem>
-          <Text>Story Map on other PADEP Monitoring</Text>
+          <Link url={'http://padep-1.maps.arcgis.com/apps/Cascade/index.html?appid=d3ad8b96c3344a2aaeb7de6f1d310329'}>
+            <Text>Story Map on other PADEP Monitoring</Text>
+          </Link>
         </ListItem>
         <ListItem>
-          <Text>PADEP Water Management</Text>
+          <Link url={'http://www.dep.pa.gov/DataandTools/Webinars/Pages/Water-Management.aspx'}>
+            <Text>PADEP Water Management</Text>
+          </Link>
         </ListItem>
         <ListItem>
-          <Text>Susquehanna River Story Map</Text>
+          <Link url={'http://www.padep-1.maps.arcgis.com/apps/Cascade/index.html?appid=226b2b211887475394097b99797a1a25'}>
+            <Text>Susquehanna River Story Map</Text>
+          </Link>
         </ListItem>
         <ListItem>
-          <Text>Water Quality Network Viewer</Text>
+          <Link url={'http://www.depgis.state.pa.us/WQN/'}>
+            <Text>Water Quality Network Viewer</Text>
+          </Link>
         </ListItem>
         <ListItem>
-          <Text>PADEP Water Quality Network</Text>
+          <Link url={'http://www.dep.pa.gov/Business/Water/CleanWater/WaterQuality/Pages/Water-Quality-Network.aspx'}>
+            <Text>PADEP Water Quality Network</Text>
+          </Link>
         </ListItem>
         <ListItem>
-          <Text>PA Open Data Portal</Text>
+          <Link url={'http://data.pa.gov'}>
+            <Text>PA Open Data Portal</Text>
+          </Link>
         </ListItem>
       </List>
     )
